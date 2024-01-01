eaeda99e19f31a00ee3ba614da0287a5537ac0d2
danibek1 <155296477+danibek1@users.noreply.github.com>
2024-01-01 21:02:25 +0200

    Add files via upload

---
+++ js/board.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function createBoard() {
+    var board = [];
+    for (var i = 0; i < size; i++) {
+        board[i] = [];
+        for (var j = 0; j < size; j++) {
+            board[i][j] = { isMine: false, isShown: false, isMarked: false, visited: false };
+        }
+    }
+    return board;
+}
+
+function renderBoard(board) {
+    var strHtml = '';
+    for (var i = 0; i < size; i++) {
+        strHtml += `<tr>`;
+        for (var j = 0; j < size; j++) {
+            strHtml += `<td 
+                    onclick="onCellClick(${i},${j}, this)" 
+                    oncontextmenu="onCellRightClick(event, ${i},${j}, this); return false;" 
+                    data-i="${i}" data-j="${j}" 
+                    class="${board[i][j].isShown ? '' : 'hidden'}">
+                    ${board[i][j].isShown ? (board[i][j].isMine ? '💥' : '') : ''}
+                </td>`;
+        }
+        strHtml += `</tr>`;
+    }
+    const elBoard = document.querySelector('.my-table');
+    elBoard.innerHTML = strHtml;
+}
